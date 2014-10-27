2dc5dae1fd6b40aa5a5cd5a0463350cb9409fe91
catlag <sammieeagle@gmail.com>
2014-10-27 15:41:02 -0700

    Added many to many association

--- app.js
+++ app.js
@@ -56,14 +56,20 @@ app.get('/', routeMiddleware.preventLoginSignup, function(req,res){
 	res.render('index');
 });
 
+
+// sign up page
 app.get('/signup', routeMiddleware.preventLoginSignup, function(req,res){
     res.render('signup', { username: ""});
 });
 
+
+// login page
 app.get('/login', routeMiddleware.preventLoginSignup, function(req,res){
     res.render('login', {message: req.flash('loginMessage'), username: ""});
 });
 
+
+// create new user
 app.post('/submit', function(req,res){
 
   db.User.createNewUser(req.body.username, req.body.password,
@@ -71,22 +77,19 @@ app.post('/submit', function(req,res){
     res.render("signup", {message: err.message, username: req.body.username});
   },
   function(success){
-    res.render("myrecipes", {message: success.message});
+    res.render("myrecipes", {message: success.message });
   });
 });
 
-
+// login
 app.post('/login', passport.authenticate('local', {
   successRedirect: '/myrecipes',
   failureRedirect: '/login',
   failureFlash: true
 }));
 
-app.get('/logout', function(req,res){
-  req.logout();
-  res.redirect('/');
-});
 
+// search for foods
 app.get('/search', function(req, res){
 
 var searchTerm = req.query.recipe;
@@ -96,27 +99,30 @@ var url = "http://api.yummly.com/v1/api/recipes?_app_id=83f9f74b&_app_key=e5effb
 request(url, function (error, response, body) {
 if (!error && response.statusCode == 200) {
       var recipes = JSON.parse(body);
-    
+
       res.render('results', {Results: recipes.matches});
     }
   });  
 });
 
+
+// show all recipes
 app.get('/myrecipes', routeMiddleware.checkAuthentication, function(req,res){
-	db.Food.findAll().done(function(err, recipes){;
-		res.render('myrecipes',{ allFoods: recipes});
-	});
-	
+
+  req.user.getFoods().done(function(err, foods){
+    res.render('myrecipes', { allFoods: foods});
+  });
+
 });
 
 
+// show recipe details
 app.get('/details/:id', function(req, res){
     var foodId = req.params.id;
     var obj;
     console.log(foodId);
 
     var url ="http://api.yummly.com/v1/api/recipe/" + foodId + "?_app_id=83f9f74b&_app_key=e5effbbe06740d184e03db23a8b71bef";
-
 
     request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -126,25 +132,32 @@ app.get('/details/:id', function(req, res){
   });
 });
 
+
+// add recipe to list
 app.post('/myrecipes/:id', function(req,res){
 	var foodId = req.params.id;
 	var name = req.body.results.name;
 	var imageUrl= req.body.results.imgUrl;
 
-	db.Food.create({
-		foodId: foodId,
-		imageUrl: imageUrl, 
-		name: name}).done(function(err,success){
-			if(err) {
-				console.log(err);
-				res.render('details');
-			}
-			else {
-				res.redirect('/myrecipes');
-			}
-		});
-	});
+console.log(req.user);
 
+  db.Food.findOrCreate({
+    where: {
+      foodId: foodId,
+      name: name,
+      imageUrl: imageUrl 
+    }
+  }).done(function(err, food, created){
+    req.user.addFood(food);
+    res.redirect('/myrecipes');
+  });
+});
+
+
+
+
+
+// delete recipe from list
 app.delete('/myrecipes/:id/delete', function(req,res){
   db.Food.find({ where: { foodId: req.params.id}}).success(function(food){
     food.destroy().success(function(){
@@ -153,6 +166,8 @@ app.delete('/myrecipes/:id/delete', function(req,res){
   }); 
 });
 
+
+// get zipcode
 app.post('/location', function(req, res){
 
 var foodId = req.body.results;
@@ -175,6 +190,8 @@ if (!error && response.statusCode == 200) {
   });  
 });
 
+
+// get all store in zipcode
 app.get('/stores', function(req, res){
 
 var zipcode = req.body.zipcode;
@@ -197,10 +214,18 @@ if (!error && response.statusCode == 200) {
 });
 
 
+// log out
+app.get('/logout', function(req,res){
+  req.logout();
+  res.redirect('/');
+});
+
+
 
 app.get('*', function(req,res){
   res.render('404');
 });
+
 
 app.listen(3000, function(){
   "Server is listening on port 3000. Yay!";
